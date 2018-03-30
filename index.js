95d6db7565969738c26b2463ebf7a255e158c93a
Alexander Urizar <urizaraj@gmail.com>
2018-03-30 13:43:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function produceDrivingRange(range) {
+  return function(start, end) {
+    let i = parseInt(start.slice(0, 2))
+    let j = parseInt(end.slice(0, 2))
+    let distance = Math.abs(i - j)
+    if (distance > range) {
+      return `${distance - range} blocks out of range`
+    } else {
+      return `within range by ${range - distance}`
+    }
+  }
+}
+
+function produceTipCalculator(tip) {
+  return function(total) {
+    return total * tip
+  }
+}
+function createDriver() {
+  let driverId = 0
+
+  return class Driver {
+    constructor(name) {
+      this.id = ++driverId
+      this.name = name
+    }
+  }
+}
