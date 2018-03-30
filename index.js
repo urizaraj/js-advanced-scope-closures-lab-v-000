3e5401ee2dfe3f5f78058b6bf51e5c88d2285e7f
Alexander Urizar <urizaraj@gmail.com>
2018-03-30 13:36:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(range) {
   return function(start, end) {
-    let i = parseInteger(start.slice(0, 2))
-    let j = parseInteger(end.slice(0, 2))
+    let i = parseInt(start.slice(0, 2))
+    let j = parseInt(end.slice(0, 2))
     return Math.abs(i - j) < range
   }
 }
